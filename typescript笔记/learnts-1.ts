81a0d41faf47e09a77694e0882d5e3605f82b7cf
galaxy-s10 <2274751790@qq.com>
2021-08-25 23:51:02 +0800

    ts练习-0825

---
+++ typescript笔记/learnts-1.ts
@@ -0,0 +1,21 @@
+/**
+ * 需求：
+ */
+export interface tab {
+  id: number;
+  name: string;
+  /**
+   * children: tab[] | []
+   * 意思是必须有children，且children要么就是空数组(注意，这里的空数组不是指的值是空数组，而是指的是[]这个类型，
+   * 而这个[]类型不接受任何参数，即约等于就是空数组)，
+   * 要么就是tab[]
+   */
+  //   children: tab[] | []
+
+  /**
+   * 意思是必须有children，且children要么就是[]，第一个参数是一个对象{a:number}，要么就是tab[]
+   * 如果希望要么是tab[]要么是[{a:number}]
+   */
+  children: { a: number }[];
+  // children: tab[] | [{ a: number }]
+}
