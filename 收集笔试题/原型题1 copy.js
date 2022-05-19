4c8e497b22e21caded15aea187430cc5d4c486a1
shuisheng <2274751790@qq.com>
2022-05-20 01:13:47 +0800

    feat:日常更新

---
+++ 收集笔试题/原型题1 copy.js
@@ -0,0 +1,13 @@
+function Foo() {
+  getName = function () {
+    console.log(1);
+  };
+  return this;
+}
+Foo.getName = function () {
+  console.log(2);
+};
+Foo.prototype.getName = function () {
+  console.log(3);
+};
+Foo.getName();
